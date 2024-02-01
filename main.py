6a909c32187fd7461c8c027d9505679b5a156479
ARileyCS <157526404+ARileyCS@users.noreply.github.com>
2024-02-01 17:22:23 -0600

    Update main.py
    finished palindrome function and output True or False

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
-    pass
+    word = list(word)
+    start = 0
+    end = -1
+
+    for i in range(len(word)//2):
+        if word[start] == word[end]:
+            start += 1
+            end -= 1
+        else:
+            return False
+    return True
+
 
 #YOUR CODE GOES HERE
+
+
+user_input = input().strip()
+user_input = "".join(user_input.split())
+
+if len(user_input) > 0:
+    print(palindrome(user_input.lower()))
